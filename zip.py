9439151c82e60d894283c1519b61620b8a439989
unknown <p208p2002@gmail.com>
2019-05-20 10:23:12 +0800

    basename

--- zip.py
+++ zip.py
@@ -3,7 +3,7 @@ import os
 import time
 
 def zipFiles(files:list):
-    fileName = str(int(time.time()))+'.zip'
+    fileName = 'RFS_'+str(int(time.time()))+'.zip'
     z = zipfile.ZipFile(fileName, "w")
     for file in files:
         dir, base_filename = os.path.split(file)
@@ -12,7 +12,7 @@ def zipFiles(files:list):
     return (os.path.join(os.getcwd(), fileName),fileName)
 
 def zipDir(path):
-    fileName = str(int(time.time()))+'.zip'
+    fileName = 'RFS_'+str(int(time.time()))+'.zip'
     zipf = zipfile.ZipFile(fileName, 'w', zipfile.ZIP_DEFLATED)
     for root, dirs, files in os.walk(path):
         for file in files:
